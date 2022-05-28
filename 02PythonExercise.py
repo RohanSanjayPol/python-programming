8e34f24b260a27a6a210cad703ceeaab5d34cbb2
RohanSanjayPol <polrohan90@gmail.com>
2022-05-28 14:17:00 +0000

    py

---
+++ 02PythonExercise.py
@@ -0,0 +1,8 @@
+# first program python print function
+# It declared like this
+print("what to print")
+
+print("this is rohan sanjay pol\n how can i help you")
+
+#string concatination
+print("recalling"+"python")
