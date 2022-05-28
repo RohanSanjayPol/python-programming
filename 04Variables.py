3d9bfaf5fa6ee9e9fb2680030b08de0f74f0d44a
RohanSanjayPol <polrohan90@gmail.com>
2022-05-28 14:54:00 +0000

    py

---
+++ 04Variables.py
@@ -0,0 +1,14 @@
+# variables in python
+
+fullName=input("Enter your Full Name: ")
+age=int(input("Enter your Age: "))
+weight=float(input("What is your Weight: "))
+
+print(fullName)
+print(age)
+print(weight)
+
+#finding the length of fullName
+
+length=len(fullName)
+print(length)
