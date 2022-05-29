c04829d428048e780d753829faf3fda82bf8554a
RohanSanjayPol <polrohan90@gmail.com>
2022-05-29 03:49:00 +0000

    py

---
+++ 06DataTypes.py
@@ -0,0 +1,18 @@
+# data types in python
+
+# int data type
+var1=1234567
+print(type(var1))
+
+# float data type
+var2=69.7
+print(type(var2))
+
+# string data type
+var3="this is rohan sanjay pol"
+print(type(var3))
+
+var4='a'
+print(type(var4))
+
+
