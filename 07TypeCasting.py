469079ede1283a455dfa5374f75e0ec007d56869
RohanSanjayPol <polrohan90@gmail.com>
2022-05-29 04:17:00 +0000

    py

---
+++ 07TypeCasting.py
@@ -0,0 +1,12 @@
+# type casting in python
+
+length =len("Rohan Sanjay Pol")
+print("your name has "+str(length)+" characters")
+print(type(length))
+
+print("10"+"10")
+print(int("10")+int("10"))
+print(int("10")+float(10))
+
+
+
