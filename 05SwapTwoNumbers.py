a842beed3078ad52e31bb0b39fd5ab5f134b375d
RohanSanjayPol <polrohan90@gmail.com>
2022-05-29 03:22:00 +0000

    py

---
+++ 05SwapTwoNumbers.py
@@ -0,0 +1,15 @@
+# swap two numbers
+
+a=int(input("Enter a first number: "))
+b=int(input("Enter a first number: "))
+print("original numbers")
+print("a=" + str(a))
+print("b=" + str(b))
+
+#swaping two numbers
+print("swapped numbers")
+c=a
+a=b
+b=c
+print("a=" + str(a))
+print("b=" + str(b))
