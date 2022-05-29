ca247bd613b32a7308528988d7ca899132a3398b
RohanSanjayPol <polrohan90@gmail.com>
2022-05-29 05:16:00 +0000

    py

---
+++ 09AssignementAndComparisonOperator.py
@@ -0,0 +1,18 @@
+# Assignement and comparison operator in python
+
+# Assignment operator
+a=5
+print(a)
+a+=6
+print(a)
+a,b,c=12,14,16
+print(a,b,c)
+
+#comaparison operators
+a,b=10,9
+print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
+print(a==b)
+print(a!=b)
