2842beae852a576b268a2dbf503650c9fdc0d156
RohanSanjayPol <polrohan90@gmail.com>
2022-06-02 12:38:00 +0000

    py

---
+++ 12MembershipOperator.py
@@ -0,0 +1,13 @@
+# membership operator in python
+
+str1="Rohan"
+print('h' in str1)
+print('an'in str1)
+print("A"in str1)
+print("A" not in str1)
+print()
+
+list=[12,14,16,18]
+print(12 in list)
+print(13 in list)
+print(13 not in list)
