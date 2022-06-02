3610ef7fc071a56195088a1667766a36bd66db80
RohanSanjayPol <polrohan90@gmail.com>
2022-06-02 12:19:00 +0000

    py

---
+++ 11IdentityOperator.py
@@ -0,0 +1,30 @@
+# identity operator
+
+a=5
+b=5
+print(a is b)
+print(a is not b)
+print(id(a))
+print(id(b))
+print(a==b)
+print()
+
+a=4
+b=6
+print(a is b)
+print(a is not b)
+print(a==b)
+print()
+
+a='5'
+b=5
+print(a is b)
+print(a is not b)
+print(a==b)
+print()
+
+a="rohan"
+b="rohan"
+print(a is b)
+print(a == b)
+
