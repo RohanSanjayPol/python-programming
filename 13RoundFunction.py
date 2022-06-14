1351ffae5c0a7e3535f7c533eb5ff679b6e7810d
RohanSanjayPol <polrohan90@gmail.com>
2022-06-14 15:49:00 +0000

    py

---
+++ 13RoundFunction.py
@@ -0,0 +1,20 @@
+# round function in python
+
+print(round(10,2))
+print(round(10))
+print(round(10.23234,2))
+print(round(10.23234))
+print(round(10.63234))
+
+#some special cases
+print(round(7.5))
+#it will return nearest even integer
+
+print(round(6.5))
+
+print(round(11.5))
+
+# some special cases
+print(round(674,-1))
+print(round(674,-2))
+print(round(674,-3))
