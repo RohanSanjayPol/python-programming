40c047e35df58e9b618589efa3136c0554516090
RohanSanjayPol <polrohan90@gmail.com>
2022-06-14 16:07:00 +0000

    py

---
+++ 14Fstring.py
@@ -0,0 +1,12 @@
+# fstring
+
+name="Rohan"
+age=22
+college="NIT Sikkim"
+
+# 1st way
+print("my name is "+name+ " and iam "+str(age)+ " old"+" my college is "+college)
+# 2nd way
+print("my name is ",name, " and iam ",(age), " old"," my college is ",college)
+# 3rd way using fstring
+print(f"my name is {name} and iam {age} old my college is {college}")
