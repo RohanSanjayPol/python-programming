661345da5b22bef90649775754bedfdf273b9f4e
RohanSanjayPol <polrohan90@gmail.com>
2022-06-15 19:57:00 +0000

    py

---
+++ 16elif.py
@@ -0,0 +1,12 @@
+# elif
+
+percentage=int(input("Enter your percentage: "))
+
+if(percentage>70):
+    print("First Class")
+elif(percentage>55):
+    print("Second class") 
+elif(percentage>35):
+    print("Third class") 
+else:
+    print("Fail")         
