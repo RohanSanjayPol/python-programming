5d953925727d5d770663122e8a67edb41bb9ef9d
RohanSanjayPol <polrohan90@gmail.com>
2022-06-15 19:29:00 +0000

    py

---
+++ 17list.py
@@ -0,0 +1,16 @@
+# list in python 
+
+roll_no=[1,2,3,4,5,6,7,8,9,10]
+print(roll_no)
+
+print(roll_no[3])
+
+stud1=[1,"krishna",7.4,"hyderabad",False]
+print(stud1)
+
+stud1[2]=6.5
+print(stud1)
+
+print(len(roll_no))
+print(roll_no)
+
