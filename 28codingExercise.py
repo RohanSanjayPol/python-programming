45d8eeed30d07e889a93b61e0e4c9775d56b39f5
RohanSanjayPol <polrohan90@gmail.com>
2022-06-30 21:00:00 +0000

    py

---
+++ 28codingExercise.py
@@ -0,0 +1,12 @@
+# WAP who will pay the bills game using list
+
+import random as rd
+names=["Rohan","Bablu","husen","mahesh"]
+paybill=rd.choice(names)
+print(f"the {paybill} will pay the bill")
+
+
+
+
+
+
