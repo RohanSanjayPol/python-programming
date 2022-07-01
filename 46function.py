e9fd2fd27d5d0e2e1876390f1ebff434ae65c266
RohanSanjayPol <polrohan90@gmail.com>
2022-07-01 16:03:00 +0000

    py

---
+++ 46function.py
@@ -0,0 +1,19 @@
+# funtions in python without parenthesis
+
+def addition():
+    a=int(input("Enter a first number: "))
+    b=int(input("Enter a second number: "))
+    c=a+b
+    print(f"The addtion fo two number is {c}")
+
+addition()    
+
+#function with parenthesis with return type
+def mul(x,y):
+    c=a*b
+    return c
+
+a=int(input("Enter a first number: "))
+b=int(input("Enter a second number: "))
+multiplication=mul(a,b)
+print(multiplication)
