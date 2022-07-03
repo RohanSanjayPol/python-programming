1474f66679c20fda1666f429b776032824767f7a
RohanSanjayPol <polrohan90@gmail.com>
2022-07-03 03:41:00 +0000

    py

---
+++ 34forElse.py
@@ -0,0 +1,16 @@
+# for else loop in python
+
+numbers=[1,2,3,4,5,6,7,8,9]
+for i in numbers:
+    print(i)
+else :
+    print('sucessfully completed')    
+
+for i in numbers:
+    if(i==5):
+        break
+    print(i)
+else:
+    print("completed sucessfully completed")   
+
+     
