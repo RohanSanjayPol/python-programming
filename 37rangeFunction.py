dc4d5219f4e50a9fea4a16308fb648e5dc748f32
RohanSanjayPol <polrohan90@gmail.com>
2022-07-26 14:26:00 +0000

    py

---
+++ 37rangeFunction.py
@@ -0,0 +1,12 @@
+# range fnction in python
+
+a=range(1,10,2)
+for i in a:
+    print(i)
+print()    
+
+# total first 100 nubmers
+total=0
+for i in range(0,101):
+    total=total+i
+print(total)    
