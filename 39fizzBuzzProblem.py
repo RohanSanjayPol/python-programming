d708e1ed27989b8883c66d167c835d101e1244c0
RohanSanjayPol <polrohan90@gmail.com>
2022-07-28 10:59:00 +0000

    py

---
+++ 39fizzBuzzProblem.py
@@ -0,0 +1,12 @@
+# fizz buzz problem
+
+for i in range(1,101):
+    if(i%3==0 and i%5==0):
+        print("fiizzBizzz")
+    elif(i%3==0):
+        print("fizz")
+    elif(i%5==0):
+        print("bizz") 
+          
+    else:
+        print(i)    
