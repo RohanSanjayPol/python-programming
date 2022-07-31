771814c5792ad89cc2d03cb40b2ec212c653a0e0
RohanSanjayPol <polrohan90@gmail.com>
2022-07-31 04:32:00 +0000

    py

---
+++ 42whileToAcessElementsFromTheList.py
@@ -0,0 +1,7 @@
+# while elements to acess the elements from the list
+lists=[1,2,3,4,54,5,6,7,88,98]
+visited_elements=[]
+while(lists):
+    print(lists)
+    lists.pop()
+    #visited_elements.append
