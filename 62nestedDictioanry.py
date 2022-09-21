756ee5ec9243d9c8ff26fb45aa66af196c6b402b
RohanSanjayPol <polrohan90@gmail.com>
2022-09-21 12:57:09 +0000

    py

---
+++ 62nestedDictioanry.py
@@ -0,0 +1,12 @@
+# nested dictionary
+
+dictionary={
+    "Student Names":{
+        "FY":["A","B","C","D"],
+        "SY":["X","Y","Z"],
+        }
+}
+
+
+for i ,j in dictionary.items():
+    print(f"{i}\n{j}\n")
