35c6d014ad4b130a8cd573e23990ed167ded07ff
RohanSanjayPol <polrohan90@gmail.com>
2022-09-21 12:57:09 +0000

    py

---
+++ 63codingExercise.py
@@ -0,0 +1,18 @@
+#coding exercise- add third dictioanry in the list
+studentData=[
+    {"Name":"Rohan",
+     "Age":22,
+     "College":"NIT Sikkim",
+     "Phone No":9307632368
+    },
+
+    {"Name":"Bablu",
+     "Age":24,
+     "College":"Shivaji University",
+     "Phone No":9699383069
+    }
+]
+print(type(studentData))
+
+
+
