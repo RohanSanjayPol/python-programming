2248040efdc594d4ab064993b55be5053ac5cb76
RohanSanjayPol <polrohan90@gmail.com>
2022-11-22 14:08:35 +0000

    py

---
+++ 67forLoop.py
@@ -0,0 +1,8 @@
+# for loop 
+words=['cat','elephent','tiger']
+for i in words:
+    print(i,len(i))
+
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+for i,j  in users.items():
+    print(i,j)    
